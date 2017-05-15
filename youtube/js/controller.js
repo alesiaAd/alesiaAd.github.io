fbb9bb5f70834417b9a796a76bbdc9125fbbbb9f
alesiaAd <alesia.kurlenia@gmail.com>
2017-05-15 21:38:51 +0300

    fixing bugs

--- youtube/js/controller.js
+++ youtube/js/controller.js
@@ -4,6 +4,7 @@ var youHelper = null;
 var controller = null;
 var maxPages = 5;
 
+
 Array.prototype.extend = function (other_array) {
     other_array.forEach(function(v) {this.push(v)}, this);    
 }
@@ -13,6 +14,10 @@ class Controller {
         this.items = [];
         this.currentPage = 0;
         var self = this;
+        this.amountPerPage = 0;
+        this.padding = parseFloat(getComputedStyle(document.getElementById('search-container')).padding);
+        this.width = 250;
+        this.margin = 10;
         detectswipe('search-container', function(element, direction) {
             console.log(element, direction)
             if(direction === 'r') {
@@ -27,17 +32,19 @@ class Controller {
             }
         })
     }
-    
+
     itemsUpdated(items) {
         this.items.extend(items);
         this.updateUI();
     }
 
     updateUI() {    
+        //console.log(parseFloat(getComputedStyle(document.getElementById('video')).margin));
         document.getElementById('search-container').innerHTML = "";
-        var maxIndex = (this.currentPage + 1) * 3;
+        this.countAmountPerPage(this.amountPerPage);
+        var maxIndex = (this.currentPage + 1) * this.amountPerPage;
         maxIndex = maxIndex > youHelper.total ? youHelper.total : maxIndex;
-        for (var i = this.currentPage * 3; i < maxIndex; i++) {
+        for (var i = this.currentPage * this.amountPerPage; i < maxIndex; i++) {
 	         var item = this.items[i];
              var vidHtml = addItem(item);
              document.getElementById('search-container').innerHTML += vidHtml;
@@ -50,7 +57,7 @@ class Controller {
         }
 
         var startPage = 0;
-        var maxPageCount = Math.ceil(youHelper.total / 3);
+        var maxPageCount = Math.ceil(youHelper.total / this.amountPerPage);
         var pagesCount = maxPageCount > maxPages ? maxPages : maxPageCount;
         if(this.currentPage - 3 < 0) {
             startPage = 0;
@@ -71,12 +78,26 @@ class Controller {
         }
     }
 
+    countAmountPerPage (amountPerPage) {
+        if (document.documentElement.clientWidth > (this.padding * 2 + (amountPerPage + 1)*this.width + this.margin * 2 * (amountPerPage + 1))) {
+            this.amountPerPage++;
+            return this.countAmountPerPage(this.amountPerPage);
+        }
+        else if (this.padding * 2 + amountPerPage*this.width + this.margin * 2 * amountPerPage  > document.documentElement.clientWidth) {
+            this.amountPerPage--;
+            return this.countAmountPerPage(this.amountPerPage);
+        }
+        else {
+            return this.amountPerPage;
+        }
+    }
+
     canMoveBack() {
         return this.currentPage > 0;
     }
 
     canMoveForward() {
-        return this.currentPage * 3 + 3 < youHelper.total;
+        return this.currentPage * this.amountPerPage + this.amountPerPage < youHelper.total;
     }
 }
 
@@ -86,7 +107,7 @@ function search() {
         youHelper = null;
     }
     if(youHelper == null) {
-        youHelper = new YouHelper(string, 15);
+        youHelper = new YouHelper(string, 30);
         youHelper.next();
         document.getElementById('search-container').innerHTML = '<img alt="loading..." src="img/loader.gif"/>';
     }
@@ -98,31 +119,59 @@ function search() {
     
 }
 
+var isClicked = false;
+
 function pagePrev() {
-    slide('ltr', function(){
-        controller.currentPage = controller.currentPage - 1;
-        controller.updateUI();
-    })
+    if (!isClicked) {
+        slide('ltr', function(){
+            controller.currentPage = controller.currentPage - 1;
+            controller.updateUI();
+        })
+        isClicked = true;
+        setTimeout(function(){
+                isClicked = false;
+        },500)
+    }   
 }
 
 function pageNext() {
-    slide('rtl', function() {
-        controller.currentPage = controller.currentPage + 1;
-        controller.updateUI();
-        if(((controller.currentPage + 2) * 3 + 1) > controller.items.length - 1) {
-            youHelper.next();
-        }
-    })
+    if (!isClicked) {
+        slide('rtl', function() {
+            controller.currentPage = controller.currentPage + 1;
+            controller.updateUI();
+            if(((controller.currentPage + 2) * controller.amountPerPage + 1) > controller.items.length - 1) {
+                youHelper.next();
+            }
+        })
+        isClicked = true;
+        setTimeout(function(){
+                isClicked = false;
+        },500)
+    }
 }
 
 function pageHandler(a) {
-    var index = parseInt(a.innerHTML) - 1;
-    var direction = controller.currentPage > index ? 'ltr' : 'rtl'
-    slide(direction, function(){
-        controller.currentPage = index;
-        controller.updateUI();
-        if(((controller.currentPage + 2) * 3 + 1) > controller.items.length - 1) {
-            youHelper.next();
+    if (!isClicked) {
+        var index = parseInt(a.innerHTML) - 1;
+        if(controller.currentPage != index) {
+            var direction = controller.currentPage > index ? 'ltr' : 'rtl'
+            slide(direction, function(){
+                controller.currentPage = index;
+                controller.updateUI();
+                if(((controller.currentPage + 2) * controller.amountPerPage + 1) > controller.items.length - 1) {
+                    youHelper.next();
+                }
+            })
+            isClicked = true;
+            setTimeout(function(){
+                isClicked = false;
+            },500) 
         }
-    })    
+    }   
+}
+
+function isEnter(event) {
+    if (event.keyCode == 13) {
+        return search();
+    }
 }
